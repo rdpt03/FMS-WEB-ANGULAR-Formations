44156e772fe7ce45360c3bb3bcac23c62f7dcbc4
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-29 09:28:35 +0100

    add back quantity

--- src/app/models/training.ts
+++ src/app/models/training.ts
@@ -5,10 +5,11 @@ export class Training {
     price : number;
     quantity : number;
 
-    constructor(id:number,name:string,description:string,price:number){
+    constructor(id:number,name:string,description:string,price:number, quantity:number){
         this.id=id;
         this.name=name;
         this.description = description;
         this.price = price;
+        this.quantity = quantity;
     }
 }
