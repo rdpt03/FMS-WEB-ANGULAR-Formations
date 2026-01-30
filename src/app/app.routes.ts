d1c5813a58a23e4a2713afd4ee0770abf5a3d2d1
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-30 13:32:38 +0100

    add cart and 404 not found routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TrainingComponent } from './components/training-component/training-component';
 import { Home } from './components/home/home';
-import { RegisterFormComonent } from './components/register-form-comonent/register-form-comonent';
+import { RegisterFormComonent } from './components/register-form-component/register-form-component';
+import { PageNotFound } from './components/page-not-found/page-not-found';
+import { CartComponent } from './components/cart-component/cart-component';
 
 export const routes: Routes = [
     {
@@ -15,5 +17,13 @@ export const routes: Routes = [
     {
         path: 'register',
         component: RegisterFormComonent
+    },
+    {
+        path: 'cart', 
+        component:CartComponent
+    },
+    {
+        path: '**',
+        component: PageNotFound
     }
 ];
