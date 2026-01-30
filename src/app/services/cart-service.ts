3f05a274ac3af216845bb3eee6d55741615b7c7a
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-30 13:27:53 +0100

    refactor entire code to fix bug that said the localStorage is not set

--- src/app/services/cart-service.ts
+++ src/app/services/cart-service.ts
@@ -1,14 +1,76 @@
 import { Injectable } from '@angular/core';
 import { Training } from '../models/training';
+import { TrainingOnList } from '../models/trainingOnCart';
+
 
 @Injectable({
     providedIn: 'root',
 })
-export class CartService {
-    constructor() { }
 
-    addTraining(training:Training){
-        console.log(training.name);
-        console.log(training.quantity);
+export class CartService {
+    private cart : TrainingOnList[] = [];
+    private readonly STORAGE_KEY = 'cart';
+    
+    constructor() {
+        if (typeof localStorage !== 'undefined') {
+            const storedCart = localStorage.getItem(this.STORAGE_KEY);
+            if (storedCart) {
+                this.cart = JSON.parse(storedCart);
+            }
+        }
+    }
+
+
+
+    /**
+    * Adds a training to the cart.
+    * If the training already exists, increments its quantity.
+    * @param training - The Training object to add
+    * @param quantity - Number of units to add (default is 1)
+    */
+    addTraining(training:Training, quantity:number){     
+        // Check if the training already exists in the cart
+        const existing = this.cart.find(
+            art => art.training.id === training.id
+        );
+
+        if (existing) {
+            // If it exists, increase the quantity
+            existing.quantity += quantity;
+        } else {
+            // If it doesn't exist, add a new entry
+            this.cart.push({ training, quantity });
+        }
+
+        //save to local storage also
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.cart));
+    }
+
+    removeTraining(training:number | Number | Training){
+        //create var to stora
+        let trainingId : number;
+
+
+        //transform into id
+        if(training instanceof Training){
+            trainingId = training.id;
+        }
+        else if(training instanceof Number ||  typeof training === 'number'){
+            trainingId = (+training);
+        }
+        else{
+            throw new Error('Invalid type for training');
+        }
+
+
+        //remove using filter function 
+        this.cart = this.cart.filter(t => t.training.id !== trainingId)
+
+        //save to local storage also
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.cart));
+    }
+
+    getCart(){
+        return this.cart;
     }
 }
