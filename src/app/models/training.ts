8b7d2e8e1bb74b484ababe3d1d7f8cc8138d540b
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-30 13:17:33 +0100

    remove quantity

--- src/app/models/training.ts
+++ src/app/models/training.ts
@@ -3,13 +3,11 @@ export class Training {
     name : string;
     description : string;
     price : number;
-    quantity : number;
 
-    constructor(id:number,name:string,description:string,price:number, quantity:number){
+    constructor(id:number,name:string,description:string,price:number){
         this.id=id;
         this.name=name;
         this.description = description;
         this.price = price;
-        this.quantity = quantity;
     }
 }
