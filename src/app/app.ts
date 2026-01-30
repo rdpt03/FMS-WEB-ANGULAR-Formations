3cffd8e30dbaef18c71e4c54e7e43ec26ad08c90
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-30 13:31:28 +0100

    add cartService

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { CartService } from './services/cart-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  constructor(){}
   protected readonly title = signal('trainings-front-app');
 }
