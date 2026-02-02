5a77dffd3f8af53b126354cdc00727bc65346984
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-02-02 13:56:29 +0100

    fix tabulation

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  imports: [RouterModule],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+    selector: 'app-home',
+    imports: [RouterModule],
+    templateUrl: './home.html',
+    styleUrl: './home.css',
 })
 export class Home {
 
