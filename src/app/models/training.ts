d3fc63952eaab92d2eeff7f8728e8db1e0e4a1bc
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-02-02 13:41:34 +0100

    add quantity

--- src/app/models/training.ts
+++ src/app/models/training.ts
@@ -3,11 +3,13 @@ export class Training {
     name : string;
     description : string;
     price : number;
+    quantity : number;
 
     constructor(id:number,name:string,description:string,price:number){
         this.id=id;
         this.name=name;
         this.description = description;
         this.price = price;
+        this.quantity = 1;
     }
 }
