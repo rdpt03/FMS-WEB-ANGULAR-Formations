d6a2e840f703b0a8d2a3de6199fe1d0d58112126
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-02-02 13:22:02 +0100

    add function to call the backend and get trainings

--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -1,8 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../Environment';
+import { Training } from '../models/training';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ApiService {
+    constructor(private http:HttpClient){}
+
+    
+    getTrainings(){
+        return this.http.get<Training[]>(environment.host+"/trainings")
+    }
+
+
+    getTrainingById(id : number){ return this.getTraining(id) }
+    getTraining(id : number){
+        return this.http.get<Training[]>(environment.host+"/trainings/"+id)
+    }
 
 }
