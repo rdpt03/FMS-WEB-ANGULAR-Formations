1176a268957391f009e9cbdaa00fc71d983ac2e2
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-02-02 13:43:07 +0100

    add call from the apiService to get the trainings

--- src/app/components/training-component/training-component.ts
+++ src/app/components/training-component/training-component.ts
@@ -4,6 +4,7 @@ import { Training } from '../../models/training';
 import { FormsModule } from '@angular/forms';
 import { CartService } from '../../services/cart-service';
 import { Router } from '@angular/router';
+import { ApiService } from '../../services/api-service';
 
 interface TrainingWithQuantity extends Training {
     quantity: number;
@@ -18,20 +19,27 @@ interface TrainingWithQuantity extends Training {
 })
 export class TrainingComponent implements OnInit{
     listTrainings : TrainingWithQuantity[] | undefined;
+    error : any = null;
     filteredTrainings : TrainingWithQuantity[] | undefined;
     searchText : string = "";
-    constructor(private cartService : CartService, private router : Router) { }
+    constructor(private apiService : ApiService, private cartService : CartService, private router : Router) { }
     
 
     ngOnInit(): void {
+        this.apiService.getTrainings().subscribe({
+            next : (data) => this.listTrainings = data,
+            error : (err) => this.error = err.message,
+            complete : () => this.error = null
+        });
+        /*
         const listTrainings = [
             {id:1, name:'Java',description:'Formation Java 8 pour 5 jours', price:1500 },
             {id:2, name:'DotNet',description:'Formation DotNet 3 jours', price:1000},
             {id:3, name:'Python',description:'Formation Python/Django 5 jours', price:1500}
-        ];
+        ];*/
 
         // Add quantityt attriute
-        this.listTrainings = listTrainings.map(t => ({ ...t, quantity: 1 }));
+        //this.listTrainings = listTrainings.map(t => ({ ...t, quantity: 1 }));
     }  
 
 
