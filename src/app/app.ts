4393c91b92aae89fa6a95afddca1eac0349134cc
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-02-03 13:55:00 +0100

    get layout status from router and pass to html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { CartService } from './services/cart-service';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -10,6 +11,24 @@ import { CommonModule } from '@angular/common';
     styleUrls: ['./app.css']
 })
 export class App {
-    constructor(public cartService : CartService){}
+    constructor(public cartService : CartService, private router: Router, private route: ActivatedRoute){}
     protected readonly title = signal('trainings-front-app');
+ 
+    showLayout = true;
+
+    ngOnInit() {
+        //get if this route should be with or without navbar and footer
+        this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe(() => {
+                let currentRoute = this.route.firstChild;
+
+                while (currentRoute?.firstChild) {
+                    currentRoute = currentRoute.firstChild;
+                }
+
+                this.showLayout = !currentRoute?.snapshot.data?.['noLayout'];
+        });
+
+    } 
 }
