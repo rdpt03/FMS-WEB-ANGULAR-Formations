357409580be192ca9ebb1561d56028cadec9c937
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-02-03 13:47:45 +0100

    add function to get user according to login

--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../Environment';
 import { Training } from '../models/training';
+import { User } from '../models/user';
 
 @Injectable({
     providedIn: 'root',
@@ -18,5 +19,14 @@ export class ApiService {
     getTraining(id : number){
         return this.http.get<Training[]>(environment.host+"/trainings/"+id)
     }
+
+    checkUser(email: string, password: string) {
+        // Return Observable, do NOT subscribe here
+        return this.http.get<User[]>(
+            environment.host + '/users?email='+email+'&password='+password,
+        );
+    }
+
+
 
 }
