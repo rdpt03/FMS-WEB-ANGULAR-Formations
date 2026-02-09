b123187176d33ebb09839c6caa7170ab1d66cd3b
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-02-09 13:54:58 +0100

    fix adminGuardSpec

--- src/app/guards/admin-guard.spec.ts
+++ src/app/guards/admin-guard.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { userGuard } from './user-guard';
+import { adminGuard } from './admin-guard';
 
-describe('userGuard', () => {
+describe('adminGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => userGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => adminGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
